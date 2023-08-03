ffcf6c4f0762aa4f764e0d794c07e4c353ab7b6d
ammatwain <amedeo@sorpreso.it>
2023-08-03 16:36:58 +0200

    2023-08-03 16:36:58 -> No Comment

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,16 @@
+import path from 'path';
+import CopyPlugin from 'copy-webpack-plugin';
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
+  }),
+  new CopyPlugin({
+      patterns: [
+          { from: path.resolve(__dirname, "src/Letture"), to: path.resolve(__dirname, ".webpack/main/Letture")},
+      ],
   }),
 ];
