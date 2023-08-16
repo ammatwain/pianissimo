a2f441ea42021eeb82fccd80e197cc709ca7cf7f
ammatwain <amedeo@sorpreso.it>
2023-08-16 16:47:34 +0200

    2023-08-16 16:47:34 -> No Comment

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import CopyPlugin from 'copy-webpack-plugin';
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import WebpackAutoInject from "webpack-auto-inject-version";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
@@ -16,5 +17,12 @@ export const plugins = [
             { from: path.resolve(__dirname, "src/Data"), to: path.resolve(__dirname, ".webpack/main/Data")},
             { from: path.resolve(__dirname, "src/pianissimo.png"), to: path.resolve(__dirname, ".webpack/main/pianissimo.png")},
         ],
+    }),
+    new WebpackAutoInject({
+        // options
+        // example:
+        components: {
+            AutoIncreaseVersion: true,
+        }
     }),
 ];
