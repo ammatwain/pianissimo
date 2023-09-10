3016ba823399ba196f63b15d36bb57506851cbb1
ammatwain <amedeo@sorpreso.it>
2023-09-10 15:10:01 +0200

    test

--- preinstall.sh
+++ preinstall.sh
@@ -1,3 +1,4 @@
 #sudo apt install g++ make pkgconf python-is-python3 libxi-dev libglu1-mesa-dev
 #npm init electron-app@latest my-new-app -- --template=webpack-typescript
 #https://www.electronforge.io/templates/typescript-+-webpack-template
+#
