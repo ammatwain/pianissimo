b4a27fda44481cce0b31af7dd8bee5b4b6956971
ammatwain <amedeo@sorpreso.it>
2023-09-14 20:38:24 +0200

    2023-09-14 20:38:24 -> No Comment

--- src/Library/Common/Interfaces/IBranchObject.ts
+++ src/Library/Common/Interfaces/IBranchObject.ts
@@ -31,3 +31,52 @@ export interface IBranchObject {
     $path?: string;
     $children?: IBranchObject[];
 }
+
+/*
+CREATE TABLE "books" (
+	"bookId"	INTEGER NOT NULL,
+	"parentBookId"	INTEGER NOT NULL,
+	"sequence"	INTEGER NOT NULL,
+	"title"	INTEGER NOT NULL,
+	PRIMARY KEY("bookId")
+);
+
+CREATE TABLE "scores" (
+	"scoreId"	INTEGER NOT NULL,
+	"bookId"	NUMERIC NOT NULL,
+	"sequence"	INTEGER NOT NULL,
+	"title"	TEXT NOT NULL,
+	"subtitle"	TEXT DEFAULT null,
+	"author"	TEXT DEFAULT null,
+	"mainKey"	INTEGER DEFAULT null,
+	"measures"	INTEGER DEFAULT null,
+	"parts"	TEXT DEFAULT null,
+	PRIMARY KEY("scoreId")
+);
+
+CREATE TABLE "sheets" (
+	"sheetId"	INTEGER NOT NULL,
+	"scoreId"	INTEGER NOT NULL,
+	"sequence"	INTEGER NOT NULL,
+	"title"	TEXT NOT NULL,
+	"subtitle"	TEXT DEFAULT null,
+	"activeKey"	INTEGER DEFAULT null,
+	"activeKeys"	TEXT DEFAULT null,
+	"measureStart"	INTEGER DEFAULT null,
+	"measureEnd"	INTEGER DEFAULT null,
+	"selectedParts"	TEXT DEFAULT null,
+	"selectedStaves"	TEXT DEFAULT null,
+	"transposeBy"	TEXT DEFAULT null,
+	"shot"	TEXT NOT NULL DEFAULT '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0',
+	"done"	TEXT NOT NULL DEFAULT '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0',
+	"loop"	TEXT NOT NULL DEFAULT '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0',
+	PRIMARY KEY("sheetId")
+);
+
+CREATE TABLE "zippedScoreFiles" (
+	"scoreId"	INTEGER NOT NULL,
+	"zippedScoreFile"	BLOB NOT NULL,
+	PRIMARY KEY("scoreId")
+);
+
+*/
